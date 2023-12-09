7c5ae99900cf14095f2e3dd53141cf19e8ae64f6
Alexander Nevarko <nevarko.ad@phystech.edu>
2023-12-09 20:38:54 +0300

    setup fix

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ setup(
     name='cranpose',
     version='0.0.1',
     install_requires=[
-        'numpy>=1.25.2',
-        'opencv-contrib-python==4.8.1.78',
-        'scipy>=1.11.2',
-        'jupyter>=1.0.0',
-        'matplotlib>=3.7.3',
-        'pykalman @ git+https://github.com/pykalman/pykalman@8d3f8e4',
+        # 'numpy>=1.25.2',
+        # 'opencv-contrib-python==4.8.1.78',
+        # 'scipy>=1.11.2',
+        # 'jupyter>=1.0.0',
+        # 'matplotlib>=3.7.3',
+        # 'pykalman @ git+https://github.com/pykalman/pykalman@8d3f8e4',
     ],
     author='Vyacheslav Martynov, NKBTech',
-    packages=find_packages(
-        where='cranpose',
-    )   
+    packages=find_packages()   
 )
